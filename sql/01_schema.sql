045e9f05d0ea1f87fa31ca59906b7b1e64b1e38b
Sachidananda Swain <sachinswain666@gmail.com>
2026-07-24 19:45:48 +0530

    Add raw_supplement_sales table to schema
    Created a table for raw supplement sales data.

--- sql/01_schema.sql
+++ sql/01_schema.sql
@@ -1,1 +1,21 @@
 
+--NutriGlobal Sales Analysis
+ --Script 1: Database Schema, Table creation & Data Import
+
+
+
+
+
+CREATE TABLE raw_supplement_sales (
+    date DATE,
+    product_name TEXT,
+    category TEXT,
+    units_sold INT,
+    price NUMERIC(10,2),
+    revenue NUMERIC(10,2),
+    discount NUMERIC(10,2),
+    units_returned INT,
+    location TEXT,
+    platform TEXT
+);
+
